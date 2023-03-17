0b9d051c4fb6b3cefc88ccc43029c7a9ae68d938
eng ghada <ghadamoataz@gmail.com>
2023-03-17 20:38:18 +0200

    first commit

---
+++ client/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Dell
+ * Date: 3/13/2023
+ * Time: 9:43 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+
+namespace client
+{
+	internal class Program
+	{
+		private static Socket s;
+		public static void Main(string[] args)
+		{
+			s = new Socket(AddressFamily.InterNetworkV6, SocketType.Dgram, ProtocolType.Udp);
+			s.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
+			s.Bind(new IPEndPoint(IPAddress.IPv6Any, 14568));
+			
+			Task t = Task.Factory.StartNew(listen);
+			
+			Console.WriteLine("-----");
+			Console.ReadKey();
+		}
+		public static void listen() {
+			
+			IPEndPoint ipep = new IPEndPoint(IPAddress.IPv6Any, 0);
+			EndPoint ep = ipep as EndPoint;
+			Byte[] data = new byte[65535];
+			s.ReceiveFrom(data, ref ep);
+			List<Byte> bytes = new List<Byte>(data);
+			bytes.RemoveAll(b=> b == 0);
+			Console.WriteLine(Encoding.ASCII.GetString(bytes.ToArray()));
+			listen();
+			
+		    //IPV4: 127.0.0.1
+		    //IPV6: 46326:235623:346346:43254
+		}
+	}
+}
---
+++ virus01/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Dell
+ * Date: 3/13/2023
+ * Time: 9:43 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace virus01
+{
+	internal class Program
+	{
+		
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to our control panel!!");
+			
+			sendCmd(Console.ReadLine());
+			
+			Console.ReadKey();
+		}
+		
+		public static void sendCmd(string cmd ){
+		
+			Socket s = new Socket(AddressFamily.InterNetworkV6, SocketType.Dgram, ProtocolType.Udp);
+			s.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
+			IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 14568);
+			Byte[] data = Encoding.ASCII.GetBytes(cmd);
+			s.SendTo(data, ipep);
+		}
+	}
+}
